ebfdb792ef524d7fbe5fd73fc9ee9d0c0b569c37
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-31 15:35:32 -0500

    c03

---
+++ src/app/c03input-properties/c03input-properties.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c03input-properties',
+  standalone: true,
+  imports: [],
+  templateUrl: './c03input-properties.component.html',
+  styleUrl: './c03input-properties.component.css'
+})
+export class C03inputPropertiesComponent {
+
+}
