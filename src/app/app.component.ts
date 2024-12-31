5450bdc05a7bf6ce13d36e966df940dd8ac86ba0
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-31 14:37:23 -0500

    First component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { C01expressionsComponent } from "./c01expressions/c01expressions.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, C01expressionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/c01expressions/c01expressions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-c01expressions',
+  standalone: true,
+  imports: [],
+  templateUrl: './c01expressions.component.html',
+  styleUrl: './c01expressions.component.css'
+})
+export class C01expressionsComponent {
+
+}
